c3cedd74451542696633d7e54067751d7b9377eb
MeiCR <90983015+MeiCR@users.noreply.github.com>
2022-04-16 00:42:07 -0500

    Revert "Create CEvento.h"

---
+++ PC1_Grupo 07/PC1_Grupo 07/CNota.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Capp.h"
+template<class t1>
+class CNota: public App<t1>
+{
+public:
+	CNota(t1 titulo, t1 descripcion):App(titulo,descripcion) {
+		this->titulo = titulo;
+		this->descripcion = descripcion;
+	}
+	void save_data(string line) {
+		fstream file("tareas.txt");
+		vector<string>data;
+		if (file.is_open()) {
+			for (int i = 0; i < 4; i++)
+			{
+				cout << "\n Ingresar tarea: ";
+				getline(cin, line);//el getline solo lee con string
+				data.push_back(line);
+			}
+		}
+		save_Datas(data);
+	}
+	void show_data(string line) {
+		fstream file("tareas.txt");
+		file.open("tareas.txt", ios::in);
+		cout << "___________________________________________________________________\n";
+		while (!file.eof()) {
+			getline(file, line);
+			cout << line << "\n";
+		}
+		cout << "___________________________________________________________________\n";
+		_getch();
+	}
+	~CNota() {}
+};
